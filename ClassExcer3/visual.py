2d65f6f796bfc470fece292d28e70e04ae7bfb75
nthieu98 <hieunt13.echy@gmail.com>
2019-10-03 13:47:12 +0700

    update data visualer

--- ClassExcer3/visual.py
+++ ClassExcer3/visual.py
@@ -1,10 +1,16 @@
 import matplotlib.pyplot as plt
-
+import numpy as np
 
 f = open("ViberStringC1.txt", "r")
-
-y = f.readline()
-plt.plot(y)
-# plt.show()
-
+x = np.arange(0, 6, 0.1)
+print(x)
+line = f.readline()
+y = []
+while line:
+  num = float(line[:-2])
+  y.append(num)
+  line = f.readline()
 print(y)
+plt.plot(x, y[:60])
+plt.show()
+
