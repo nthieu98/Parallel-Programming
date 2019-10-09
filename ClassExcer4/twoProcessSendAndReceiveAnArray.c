f3bff507f5704e396b61719746aff6bc78a3f624
Nguyen Trung Hieu <hieu@nhom164813.com>
2019-10-09 16:51:31 +0700

    MPI demo

---
+++ ClassExcer4/twoProcessSendAndReceiveAnArray.c
@@ -0,0 +1,24 @@
+#include "mpi.h"
+#include <stdio.h>
+
+const int N = 15;
+
+int main(int argc, char ** argv)
+{
+  int rank, size;
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPI_Status stat;
+  int s[N] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+  int r[N];
+  MPI_Send(s, 10, MPI_INT, 1 - rank, rank, MPI_COMM_WORLD);
+  MPI_Recv(r, 10, MPI_INT, 1 - rank, 1 - rank, MPI_COMM_WORLD, &stat);
+  printf("Process %d received: ", rank);
+  for(int i = 0; i < 10; i++){
+    printf("%d ", r[i]);
+  }
+  printf("\n");
+  MPI_Finalize();
+  return 0;
+}
