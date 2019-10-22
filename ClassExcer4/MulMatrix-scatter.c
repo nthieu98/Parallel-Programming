1036f581eb40dcac9995cf36e31e955ed04e5f24
nthieu98 <hieunt13.echy@gmail.com>
2019-10-22 23:38:13 +0700

    Matrix Multiplication Scatter Gather Bcast

--- ClassExcer4/MulMatrix-scatter.c
+++ ClassExcer4/MulMatrix-scatter.c
@@ -4,44 +4,52 @@
 
 int main(int argc, char **argv)
 {
-    int m, n;
-    m = 10;
+    int m, n, p;
+    m = 20;
     n = 10;
+    p = 20;
     int rank, size, seg;
-    int *A = (int *) malloc(n * m * sizeof(int));
-    int *B = (int *) malloc(n * m * sizeof(int));
-    int *C = (int *) malloc(n * ms * sizeof(int));
+    int *A = (int *) malloc(m * n * sizeof(int));
+    int *B = (int *) malloc(n * p * sizeof(int));
+    int *C = (int *) malloc(m * p * sizeof(int));
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     seg = m / size;
     MPI_Status stt;
     if(rank == 0) { 
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                A[i * n + j] = 1;
+            }
+        }
         for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                A[i * n + j] = i * n + j;
-                B[i * n + j] = i * n + j;
+            for(int j = 0; j < p; j++) {
+                B[i * p + j] = 1;
             }
         }
     }
-    int *Aa = (int *) malloc(seg * m * sizeof(int));
-    int *Bb = (int *) malloc(n * m * sizeof(int));
-    int *Cc = (int *) malloc(seg * m * sizeof(int));
-    MPI_Scatter(A, seg * m, MPI_INT, Aa, seg * m, MPI_INT, 0, MPI_COMM_WORLD); 
-    MPI_Bcast(B, n * m, MPI_INT, 0, MPI_COMM_WORLD);
+    int *Aa = (int *) malloc(seg * n * sizeof(int));
+    int *Bb = (int *) malloc(n * p * sizeof(int));
+    int *Cc = (int *) malloc(seg * p * sizeof(int));
+    MPI_Scatter(A, seg * n, MPI_INT, Aa, seg * n, MPI_INT, 0, MPI_COMM_WORLD); 
+    MPI_Bcast(B, n * p, MPI_INT, 0, MPI_COMM_WORLD);
     // MPI_Scatter(B, seg * n, MPI_INT, Bb, seg * n, MPI_INT, 0, MPI_COMM_WORLD);
     // printf("%d\n", rank);
     for(int i = 0; i < seg; i++) {
-        for(int j = 0; j < n; j++) {
+        for(int j = 0; j < p; j++) {
+            Cc[i * p + j] = 0;
+            for(int k = 0; k < n; k++){
+                Cc[i * p + j] += Aa[i * n + k] * Bb[k * p + j];
+            }
             // printf("%d ", Aa[i * n + j]);
-            Cc[i * n + j] = Aa[i * n + j] + Bb[i * n + j];
         }
     }
-    MPI_Gather(Cc, seg * n, MPI_INT, C, seg * n, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Gather(Cc, seg * p, MPI_INT, C, seg * p, MPI_INT, 0, MPI_COMM_WORLD);
     if(rank == 0) {
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                printf("%d ", C[i * n + j]);
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < p; j++) {
+                printf("%d ", C[i * p + j]);
             // Cc[i * n + j] = Aa[i * n + j] + Bb[i * n + j];
             }
             printf("\n");
